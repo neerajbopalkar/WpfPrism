ec139ee5a6a37b1112558138a4dd87a97b3f77bd
Neeraj Bopalkar <neeraj.bopalkar@gmail.com>
2017-04-02 11:48:53 +0530

    completed $http

--- Angular/2/script.js
+++ Angular/2/script.js
@@ -1,11 +1,34 @@
 // Code goes here
 //IIFE
+
 (function(){
 
 var app = angular.module("githubViewer", []);
 console.log("in IIFE");
 
-var MainController = function(){
+var MainController = function($scope, $http){
 	console.log("In main controller");
+
+	$scope.message = "Hello Angular";
+
+
+
+	var onUserComplete = function(response){
+
+		console.log("in onUserComplete");
+
+		$scope.user = response.data;
+
+	};
+
+	var onError1 = function(reason){
+		$scope.error = "Error occurred: " + reason.statusText;
+	};
+
+		$http.get("https://api.github.com/users/neerajbopalkar")
+			.then(onUserComplete, onError1);
+
 };
+
+app.controller("MainController", ["$scope", "$http", MainController]);
 }());
